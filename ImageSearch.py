519e46cf549d54ab1eab82c4b2b75cf2f74c5171
William Ciesialka <wciesialka@gmail.com>
2020-03-25 01:21:00 -0400

    Added ImageSearch class, which functions as a wrapper of sorts for the Google Custom Search JSON API

---
+++ ImageSearch.py
@@ -0,0 +1,27 @@
+import html, re, requests
+
+class ImageSearch:
+
+    WHITESPACE_REGEX = re.compile(r"\s+",re.MULTILINE)
+    URL = r"https://www.googleapis.com/customsearch/v1"
+
+    def __init__(self,key:str, cx:str):
+        if not isinstance(key,str):
+            raise TypeError(f"key should be type str, not {type(key)}")
+        elif not isinstance(cx,str):
+            raise TypeError(f"cx should be type str, not {type(cx)}")
+        else:
+            self.key = key
+            self.cx = cx
+
+    def search(self,query):
+        q = query.strip()
+        q = html.escape(q)
+        q = ImageSearch.WHITESPACE_REGEX.sub("+",q)
+
+        params = {'key': self.key, "cx": self.cx, "q": q}
+        r = requests.get(url=ImageSearch.URL,params=params)
+
+        data = r.json()
+
+        return data
