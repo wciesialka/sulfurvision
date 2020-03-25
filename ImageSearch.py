5c016a24433f2bc5417c58e40f39f469b5f8201a
William Ciesialka <wciesialka@gmail.com>
2020-03-25 02:11:05 -0400

    Better error handling for ImageSearch

--- ImageSearch.py
+++ ImageSearch.py
@@ -8,18 +8,23 @@ class ImageSearch:
     def __init__(self,key:str, cx:str):
         if not isinstance(key,str):
             raise TypeError(f"key should be type str, not {type(key)}")
-        elif not isinstance(cx,str):
+        if not isinstance(cx,str):
             raise TypeError(f"cx should be type str, not {type(cx)}")
-        else:
-            self.key = key
-            self.cx = cx
+        self.key = key
+        self.cx = cx
 
-    def search(self,query):
+    def search(self,query:str,start:int):
+        if not isinstance(query,str):
+            raise TypeError(f"query should be type str, not {type(query)}")
+        if not isinstance(start,int):
+            raise TypeError(f"start should be type int, not {type(start)}")
+        if start < 1:
+            raise ValueError(f"start should be >= 1, not {start}")
         q = query.strip()
         q = html.escape(q)
         q = ImageSearch.WHITESPACE_REGEX.sub("+",q)
 
-        params = {'key': self.key, "cx": self.cx, "q": q, "searchType": 'image'}
+        params = {'key': self.key, "cx": self.cx, "q": q, "searchType": 'image', start: start}
         r = requests.get(url=ImageSearch.URL,params=params)
 
         data = r.json()
