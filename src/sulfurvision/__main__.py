1c76773d500a4649d70c0f0b6f20332b52184048
Willow Ciesialka <wciesialka@gmail.com>
2023-01-07 23:18:29 -0500

    License update.

--- src/sulfurvision/__main__.py
+++ src/sulfurvision/__main__.py
@@ -1,9 +1,20 @@
+# This file is part of sulfurvision.
+# sulfurvision is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, version 3 of the License.
+# sulfurvision is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with sulfurvision. If not, see <https://www.gnu.org/licenses/>.
+
 import sulfurvision.image_search as image_search
 import sulfurvision.image_maker as image_maker
-from PIL import Image
+import argparse
 
 def main():
-    pass
+    parser = argparse.ArgumentParser(description = "Generate a Sulfur Vision image.")
+    parser.add_argument('-o', '--output', type = argparse.FileType('wb'), help = "Output file.")
+    parser.add_argument('query', help = 'Search query.', type = str)
+    args = parser.parse_args()
 
 if __name__=="__main__":
     main()
